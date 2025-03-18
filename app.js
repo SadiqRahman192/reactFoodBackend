4a90568cc22b189aeb287f4c4f47af967a1d6942
sadiqrehman <sadiqrahman192@gmail.com>
2025-03-17 22:43:39 -0700

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use((req, res, next) => {
 app.get('/meals', async (req, res) => {
   const meals = await fs.readFile('./data/available-meals.json', 'utf8');
   res.json(JSON.parse(meals));
+  res.json("HELLO FROM SERVER!!")
 });
 
 app.get('/meals/:id', async (req, res) => {
